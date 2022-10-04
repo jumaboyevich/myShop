ef4a2568a1dade5373f4d50d9c25bcd5469e9e42
ilhomjon <catchy977@gmail.com>
2022-10-04 14:18:12 +0500

    Start Project

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+              primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: const Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
