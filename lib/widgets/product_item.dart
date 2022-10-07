63037eaae3806bd41dc6567a32bf29eceb81e171
ilhomjon <catchy977@gmail.com>
2022-10-07 15:10:59 +0500

    6. Adding Navigation to the App

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -11,21 +12,11 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: const BorderRadius.all(Radius.circular(10)),
-      child: GridTile(
-        footer: GridTileBar(
-          title: Text(title),
-          backgroundColor: Colors.black87,
-          trailing: IconButton(
-            color: Theme.of(context).accentColor,
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {},
-          ),
-          leading: IconButton(
-            color: Theme.of(context).accentColor,
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
-          ),
-        ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context)
+              .pushNamed( ProductDetailScreen.routeName, arguments: id);
+        },
         child: Image.network(
           imageUrl,
           fit: BoxFit.cover,
@@ -34,3 +25,24 @@ class ProductItem extends StatelessWidget {
     );
   }
 }
+
+// child: GridTile(
+// footer: GridTileBar(
+// title: Text(title),
+// backgroundColor: Colors.black87,
+// trailing: IconButton(
+// color: Theme.of(context).accentColor,
+// icon: const Icon(Icons.shopping_cart),
+// onPressed: () {},
+// ),
+// leading: IconButton(
+// color: Theme.of(context).accentColor,
+// icon: const Icon(Icons.favorite),
+// onPressed: () {},
+// ),
+// ),
+// child: Image.network(
+// imageUrl,
+// fit: BoxFit.cover,
+// ),
+// ),
